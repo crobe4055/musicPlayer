0af2ae4999e14e0f6c8d055ea7741e1bd49f593b
Mark Ammari <mark.ammari@gmail.com>
2018-01-12 12:21:43 -0500

    fix play button, fixed overlaping, need to fix play button to autoplay music

--- javascript.js
+++ javascript.js
@@ -54,60 +54,52 @@
 // 	song.currentTime = 0;	
 // }
 
+
+
 function JukeBox(){
 
 }
 
 JukeBox.prototype.PlaySound = function(melody) {
-	var song = document.getElementById(melody);
+	var playlist = new Array('song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3');
+	var song = document.getElementById('song');
 	document.getElementById('turntable').style.backgroundImage = 'url(SongPlaying.gif)';
+	song.src = playlist[melody];
 	song.play();
 };
-JukeBox.prototype.PauseSound = function(melody) {
-	var song = document.getElementById(melody);
+JukeBox.prototype.PauseSound = function() {
+	var song = document.getElementById('song');
 	document.getElementById('turntable').style.backgroundImage = 'url(SongPaused.jpg)';
 	song.pause();
 };
-JukeBox.prototype.MuteSong = function(melody) {
-	var mute = document.getElementById(melody);
+JukeBox.prototype.MuteSong = function() {
+	var mute = document.getElementById('song');
 	mute.muted = true;
 
 };
-JukeBox.prototype.UnmuteSong = function(melody) {
-	var unmute = document.getElementById(melody);
+JukeBox.prototype.UnmuteSong = function() {
+	var unmute = document.getElementById('song');
 	unmute.muted = false;
 };
-JukeBox.prototype.Reset = function(melody) {
-	var song = document.getElementById(melody)
+JukeBox.prototype.Reset = function() {
+	var song = document.getElementById('song')
 	song.currentTime = 0;	
 };
-JukeBox.prototype.PlayRewind = function(melody) {
-	var backward = document.getElementById(melody)
+JukeBox.prototype.PlayRewind = function() {
+	var backward = document.getElementById('song')
 	document.getElementById('turntable').style.backgroundImage = 'url(SongPlaying.gif)';
 	backward.currentTime -=5.0;
 };
-JukeBox.prototype.PlayFastFoward = function(melody) {
-	var forward = document.getElementById(melody)
+JukeBox.prototype.PlayFastFoward = function() {
+	var forward = document.getElementById('song')
 	document.getElementById('turntable').style.backgroundImage = 'url(SongPlaying.gif)';
 	forward.currentTime +=5.0;		
 };
 JukeBox.prototype.PlayButton = function() {
+	var song = document.getElementById('song');
 	document.getElementById('turntable').style.backgroundImage = 'url(SongPlaying.gif)';
-	var audio = new Audio(),
-	i = 0;
-	var playlist = new Array('song1', 'song2');	
-audio.addEventListener('ended', function () {
-    i = ++i < playlist.length ? i : 0;
-    console.log(i)
-    audio.src = playlist[i];
-    audio.play();
-
-}, true);
-audio.loop = false;
-audio.src = playlist[0] ;
-audio.play();
+	song.play();
 };
-
 
 var playSong = new JukeBox();
 var pauseSong = new JukeBox();
@@ -128,5 +120,4 @@ var playButton = new JukeBox();
     
 //     document.getElementById("currentTime").innerHTML = aud.currentTime;
 
-
-// Add CommentCollapse
+// }
